7681abd324924c16e629de8babfcf6be86d49831
AlphaKeks <alphakeks@dawn.sh>
2025-02-10 04:02:53 +0100

    Add Flarum module

---
+++ modules/flarum.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  services.flarum = {
+    enable = true;
+    forumTitle = "CS2KZ Forum";
+    domain = "forum.cs2kz.org";
+    adminUser = "schnose";
+    adminEmail = "cs2kz@dawn.sh";
+    initialAdminPassword = "csgo-kz-is-dead-boys";
+  };
+}
